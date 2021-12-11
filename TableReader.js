0205c4e940c44cb7ca1612e85c708f57412c3629
Ben Lugasi <benlugasi@campus.technion.ac.il>
2021-12-11 18:37:15 +0200

    Row Click event added

--- TableReader.js
+++ TableReader.js
@@ -1,19 +1,19 @@
-// import * as Papa  from "./papaparse/papaparse.js";
-
-export function BuildTable(csv_input_id, table_id) {
+export function BuildElements(csv_input_id, table_id, map_id) {
     const table_element = document.getElementById(table_id);
+
     function handleFile() {
         const file = this.files[0];
         Papa.parse(file, {
             header: true,
             complete: function (results, file) {
-                new Tabulator(table_element, {
+                var table = new Tabulator(table_element, {
                     data: results.data,
+                    layout:"fitDataStretch",
                     height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
                     pagination: "local",       //paginate the data
                     paginationSize: 5,       //allow 5 rows per page of data
                     columns: [ //Define Table Columns
-                        {title: "Name", field: "name", width: 150},
+                        {title: "Name", field: "name", width: 150, headerFilter:"input"},
                         {title: "Host ID", field: "host_id", width: 150},
                         {title: "ID", field: "id", width: 150},
                         {title: "Neighborhood", field: "neighbourhood", width: 150},
@@ -21,7 +21,8 @@ export function BuildTable(csv_input_id, table_id) {
                         {title: "Price", field: "price", width: 150},
                     ],
                 });
-                var mymap = L.map('mapid').setView([49.9456399, 11.5713346], 13);
+
+                var map = L.map(map_id).setView([49.9456399, 11.5713346], 13);
                 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
                     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                     maxZoom: 18,
@@ -29,7 +30,11 @@ export function BuildTable(csv_input_id, table_id) {
                     tileSize: 512,
                     zoomOffset: -1,
                     accessToken: 'pk.eyJ1Ijoib2ZpcjUxMTk5NiIsImEiOiJja3d6YmNzeG4wdXgxMm91cnQ4Y3NicWFuIn0.ajaFrHOpgyqznPKfD3cpJw'
-                }).addTo(mymap);
+                }).addTo(map);
+
+                table.on("rowClick", function(e, row){
+                    map.setView([row.getData().latitude, row.getData().longitude],13)
+                });
             }
         });
     }
