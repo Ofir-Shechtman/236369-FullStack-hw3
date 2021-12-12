87165aeeec0ff73fa1de4f22dc049484c4163aed
Ben Lugasi <benlugasi@campus.technion.ac.il>
2021-12-12 12:17:14 +0200

    cosmetic changes

--- hw3.js
+++ hw3.js
@@ -7,14 +7,14 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
     const download_element = document.getElementById(download_id);
     const paginationSize = 10
     let table = new Tabulator(table_element, {
-        // data: results.data,
         index: "id",
         selectable:1,
+        downloadRowRange: "all",
         layout:"fitDataStretch",
-        height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-        pagination: "local",       //paginate the data
-        paginationSize: paginationSize,       //allow 5 rows per page of data
-        columns: [ //Define Table Columns
+        height: 205,
+        pagination: "local",
+        paginationSize: paginationSize,
+        columns: [
             {title: "Name", field: "name", width: 150, headerFilter:"input"},
             {title: "Host ID", field: "host_id", width: 150},
             {title: "ID", field: "id", width: 150},
@@ -33,13 +33,13 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
         accessToken: 'pk.eyJ1Ijoib2ZpcjUxMTk5NiIsImEiOiJja3d6YmNzeG4wdXgxMm91cnQ4Y3NicWFuIn0.ajaFrHOpgyqznPKfD3cpJw'
     }).addTo(map);
     var markers_group = L.layerGroup();
-
+    var sel_icon = L.icon({iconUrl: 'icons/marker_sel.png'})
+    var not_sel_icon = L.icon({iconUrl: 'icons/marker_not_sel.png', iconSize: [32, 32]})
 
 
     function handleFile() {
         let cur_loc = undefined;
         let mouseup = undefined;
-        let markers = {}
         input_element.style.visibility = "hidden"
         table_element.style.visibility = "visible"
         map_element.style.visibility = "visible"
@@ -52,27 +52,22 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
             skipEmptyLines: true,
             complete: function (results, file) {
                 table.setData(results.data)
-                const first_record = [results.data[0].latitude, results.data[0].longitude];
-                map.setView(first_record, 10);
-
-
                 function _rowSelected(row) {
                     table.selectRow(parseInt(row.getData()['id']))
                     table.scrollToRow(row.getIndex(), "center", true);
-                    map.flyTo([row.getData().latitude, row.getData().longitude],15,{noMoveStart:true})
                     let latlng =L.latLng(row.getData().latitude, row.getData().longitude)
+                    map.flyTo(latlng,15,{noMoveStart:true})
                     markers[latlng.toString()].marker.setIcon(sel_icon)
                     info_element.innerHTML = JSON.stringify(row.getData(), undefined, '\t');
                 }
-
-
-                table.on("rowSelected", _rowSelected)
 
                 function _rowDeselected(row){
                     let latlng =L.latLng(row.getData().latitude, row.getData().longitude)
                     markers[latlng.toString()].marker.setIcon(not_sel_icon)
                     info_element.innerHTML = "";
                 }
+
+                table.on("rowSelected", _rowSelected)
                 table.on("rowDeselected", _rowDeselected)
 
                 function _moveMapHandler(event) {
@@ -91,10 +86,8 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
                     if (same_marker) return;
                     table.setPage(parseInt(row.getPosition(true)/paginationSize+1))
                     table.selectRow(parseInt(id))
-
-
-
                 }
+
                 function _mouseDown(event) {
                     cur_loc = map.getCenter()
                 }
@@ -110,12 +103,13 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
                         _moveMapHandler(event)
                     }
                 }
+
+                const first_record = [results.data[0].latitude, results.data[0].longitude];
+                map.setView(first_record, 10);
                 map.on('mousedown',_mouseDown)
                 map.on('mouseup',_mouseUp)
                 // map.on('movestart',_moveMapHandler)
-
-                var sel_icon = L.icon({iconUrl: 'icons/marker_sel.png'})
-                var not_sel_icon = L.icon({iconUrl: 'icons/marker_not_sel.png', iconSize: [32, 32]})
+                let markers = {}
                 for (const record of results.data) {
                     let marker = L.marker([record.latitude, record.longitude], {title:record.name, riseOnHover:true, icon:not_sel_icon});
                     markers_group.addLayer(marker)
@@ -129,7 +123,6 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
     function clearData(){
         input_element.value = ""
         input_element.style.visibility = "visible"
-        var table = Tabulator.findTable(table_element)[0];
         table.clearData()
         table_element.style.visibility = "hidden"
         map_element.style.visibility = "hidden"
@@ -138,11 +131,10 @@ export function BuildElements(csv_input_id, table_id, map_id, info_id, clear_id,
         info_element.style.visibility = "hidden"
         clear_element.style.visibility = "hidden"
         download_element.style.visibility = "hidden"
-        //BuildElements(csv_input_id, table_id, map_id, info_id, clear_id, download_id)
     }
 
     function downloadData(){
-        console.log("download")
+        table.download("csv", "data.csv");
     }
     document.getElementById(csv_input_id).addEventListener("change", handleFile, false);
     document.getElementById(clear_id).addEventListener("click", clearData, false);
